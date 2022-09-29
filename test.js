2e604dd33381e014c6a3810c0aa0d306707f0c68
Rebonace <celiarebonato@gmail.com>
2022-09-29 15:31:01 +0200

    chalk commit

---
+++ test.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+const log = console.log;
+
+log(chalk.magenta.bold('Victor'));
+log(chalk.green.underline('Johanna'));
+log(chalk.yellow.bgRed('Audrey'));
+log(chalk.blue.italic('Pascaline'));
+
+
+
